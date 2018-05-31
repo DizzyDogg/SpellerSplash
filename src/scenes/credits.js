052fefe8545ec1e0bd211d766e30cc4683085c47
Daniel Fackrell <unlearned@gmail.com>
2018-05-31 12:37:40 -0600

    Added phaser3 logo and link to credits scene

--- src/scenes/credits.js
+++ src/scenes/credits.js
@@ -4,11 +4,13 @@ export default class CreditsScene extends Phaser.Scene {
     }
 
     preload () {
+        this.load.image('phaser3-logo', 'phaser3-logo.png');
     }
 
     create () {
 	var centerX = this.sys.game.config.width / 2;
 	var centerY = this.sys.game.config.height / 2;
+
         var creditsTitle = this.add.text(
 	    centerX,
 	    100,
@@ -20,5 +22,15 @@ export default class CreditsScene extends Phaser.Scene {
             this.input.stopPropagation();
             this.scene.start('MainMenu');
         });
+
+	var phaserLogo = this.add.image(centerX, 200, 'phaser3-logo');
+	phaserLogo.setInteractive();
+	phaserLogo.on('pointerdown', () => {
+            this.input.stopPropagation();
+	    // This appears to work for desktop browsers. I hope it will
+	    // request an external browser on mobile devices.
+	    window.open('http://phaser.io', '_system');
+	})
+
     }
 };
