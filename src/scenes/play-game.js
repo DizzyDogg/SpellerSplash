7f0c03b93f42d215fb08cf4c5a99cad1ffbcf4a2
Travis Chase <travis.chase@gmail.com>
2018-05-31 12:55:23 -0600

    fixed title variable, using let instead of var and fixed select letter

--- src/scenes/play-game.js
+++ src/scenes/play-game.js
@@ -1,4 +1,4 @@
-//import SelectLetter from '../interfaces/select-letter';
+import SelectLetter from '../interfaces/select-letter';
 
 export default class PlayGameScene extends Phaser.Scene {
     constructor (config, key = 'PlayGame') {
@@ -10,25 +10,25 @@ export default class PlayGameScene extends Phaser.Scene {
     }
 
     init (data) {
-	//this.selectLetter = new SelectLetter(this);
-	this.grade = data.grade;
+	    this.selectLetter = new SelectLetter(this);
+	    this.grade = data.grade;
     }
 
     create () {
-	var centerX = this.sys.game.config.width / 2;
-	var centerY = this.sys.game.config.height / 2;
-        var title = this.add.text( centerX, 100, 'Play Scene', {fontFamily: 'Arial', fontSize: 32, color: '#f00'}).setOrigin(0.5);
-        var title = this.add.text( centerX, 200, this.grade + ' grade', {fontFamily: 'Arial', fontSize: 32, color: '#f00'}).setOrigin(0.5);
-        var mainMenu = this.add.text( centerX, 300, 'Main Menu', {fontFamily: 'Arial', fontSize: 32, color: '#f00'}).setOrigin(0.5);
+	    let centerX = this.sys.game.config.width / 2;
+    	let centerY = this.sys.game.config.height / 2;
+        let title = this.add.text( centerX, 100, 'Play Scene', {fontFamily: 'Arial', fontSize: 32, color: '#f00'}).setOrigin(0.5);
+        let title1 = this.add.text( centerX, 200, this.grade + ' grade', {fontFamily: 'Arial', fontSize: 32, color: '#f00'}).setOrigin(0.5);
+        let mainMenu = this.add.text( centerX, 300, 'Main Menu', {fontFamily: 'Arial', fontSize: 32, color: '#f00'}).setOrigin(0.5);
         mainMenu.setInteractive();
         mainMenu.on('pointerdown', () => {
             this.input.stopPropagation();
             this.scene.start('MainMenu');
         });
 
-	//this.selectLetter.events.on('keypress', (key) => {
-	//	console.log(key);
-	//}
+	    this.selectLetter.events.on('keypress', (key) => {
+	        console.log(key);
+	    });
     }
 
     update () {
