f160dec445bf8b57e916d5d79591c4bbaf52af77
Matt Swensen <matthswen@gmail.com>
2018-05-31 12:38:31 -0600

    Nine grades listed and they take you to the play page and pass along which grade was pressed. Also commented out some code in the play scene which wasn't working

--- src/scenes/play-game.js
+++ src/scenes/play-game.js
@@ -1,4 +1,4 @@
-import SelectLetter from '../interfaces/select-letter';
+//import SelectLetter from '../interfaces/select-letter';
 
 export default class PlayGameScene extends Phaser.Scene {
     constructor (config, key = 'PlayGame') {
@@ -9,14 +9,26 @@ export default class PlayGameScene extends Phaser.Scene {
         // load all the resources required for this scene before using them
     }
 
-    init () {
-        this.selectLetter = new SelectLetter(this);
+    init (data) {
+	//this.selectLetter = new SelectLetter(this);
+	this.grade = data.grade;
     }
 
     create () {
-        this.selectLetter.events.on('keypress', (key) => {
-            console.log(key);
+	var centerX = this.sys.game.config.width / 2;
+	var centerY = this.sys.game.config.height / 2;
+        var title = this.add.text( centerX, 100, 'Play Scene', {fontFamily: 'Arial', fontSize: 32, color: '#f00'}).setOrigin(0.5);
+        var title = this.add.text( centerX, 200, this.grade + ' grade', {fontFamily: 'Arial', fontSize: 32, color: '#f00'}).setOrigin(0.5);
+        var mainMenu = this.add.text( centerX, 300, 'Main Menu', {fontFamily: 'Arial', fontSize: 32, color: '#f00'}).setOrigin(0.5);
+        mainMenu.setInteractive();
+        mainMenu.on('pointerdown', () => {
+            this.input.stopPropagation();
+            this.scene.start('MainMenu');
         });
+
+	//this.selectLetter.events.on('keypress', (key) => {
+	//	console.log(key);
+	//}
     }
 
     update () {
