a2c92ad45f7f6363dc247262b25aa97241e669c3
Matt Swensen <matthswen@gmail.com>
2018-05-31 14:31:29 -0600

    Added core directory. Added word-checker class to keep track of the progress the player has made and to check if a guess is right

---
+++ src/core/word-checker.js
@@ -0,0 +1,37 @@
+export default class WordChecker {
+    constructor (word) {
+        this.word = word;
+        this.progress = 0;
+        this.fails = 0;
+    }
+
+    guess_letter (letter) {
+        let actualLetter = this.word.charAt(this.progress);
+        if (actualLetter.match(letter)) {
+            this.progress++;
+            this.events.emit('guessCorrect', get_word_progress());
+        }
+        else {
+            this.events.emit('guessFail', get_word_progress());
+            this.fails++;
+        }
+    }
+
+    get_failures () {
+        return this.fails;
+    }
+
+    get_word () {
+        return this.word;
+    }
+
+    get_word_progress () {
+        let unknowns = this.word.length - this.progress;
+        let wordSoFar = this.word.substring(0, this.progress);
+        //Should I be adding underscores for the still unknown letters?
+        for (unknowns > 0; unknowns--) {
+            wordSoFar + '_';
+        }
+        return wordSoFar;
+    }
+}
